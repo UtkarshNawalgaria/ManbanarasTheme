86624068057d8a9a009083e251a176811cab8044
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2023-07-05 12:50:31 +0530

    theme updates

--- assets/custom.js
+++ assets/custom.js
@@ -1,15 +1,15 @@
-jQuery_T4NT(document).ready(function($) {
+jQuery_T4NT(document).ready(function ($) {
+  /**
+   *  Variant selection changed
+   *  data-variant-toggle="{{variant.id}}"
+   */
+  $(document).on("variant:changed", function (evt) {
+    // console.log( evt.currentVariant );
+    // $('[data-variant-toggle]').hide(0);
+    // $('[data-variant-toggle="'+evt.currentVariant.id+'"]').show(0);
+  });
 
-     /**
-     *  Variant selection changed
-     *  data-variant-toggle="{{variant.id}}"
-     */
-	   $( document ).on( "variant:changed", function( evt ) {
-	     // console.log( evt.currentVariant );
-	     // $('[data-variant-toggle]').hide(0);
-	     // $('[data-variant-toggle="'+evt.currentVariant.id+'"]').show(0);
-	   });
-
+  document.querySelectorAll(".t4s-product__description span").forEach((ele) => {
+    ele.removeAttribute("style");
+  });
 });
-
-
